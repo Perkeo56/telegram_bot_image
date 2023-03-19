81a371c87be3d5a81e4cc248fc6f3f75495a2e84
Florian Gruber <csaz8390@student.uibk.ac.at>
2023-03-19 16:36:24 +0100

    hidden api key

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ WORKDIR /app
 COPY requirements.txt requirements.txt
 RUN pip3 install -r requirements.txt
 
-COPY . .
+COPY . /app
 
 #CMD [ "python3", "-m" , "flask", "run", "--host=0.0.0.0"]
-CMD ["python3", "main.py"]
+CMD ["python3", "/app/main.py"]
--- main.py
+++ main.py
@@ -57,8 +57,9 @@ try:
         /apache_res -> Restart apache2 service.
         /start_pc -> Start PC Innsbruck.
         '''
-    api_key = "5184299088:AAHmvs1-caQXm4FnkkIqxpD0RUgx7H4DZ-I"
-    user_id = "66513226"
+
+    api_key = os.getenv(['TELEGRAM_API_KEY'])
+    user_id = os.getenv(['TELEGRAM_USER_ID'])
     # https://api.telegram.org/bot5184299088:AAHmvs1-caQXm4FnkkIqxpD0RUgx7H4DZ-I/sendMessage?chat_id=66513226&text=hi
 
     start_text = f"Bot started.\n\n {help_message}"
